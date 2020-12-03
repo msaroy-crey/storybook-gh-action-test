dae63363f75cded0c5b6a705d915a3f930b3d1c3
msaroy-crey <msaroy-crey@users.noreply.github.com>
2020-12-03 09:00:35 +0000

    Deploying to master from  @ ce25f1c4182b5c1e9f4e5d76a7cf84d5dc3080fd 🚀

--- dist/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports.Button = void 0;
-var _componentsButton = './components/Button',
-    Button = _componentsButton.Button;
-exports.Button = Button;
