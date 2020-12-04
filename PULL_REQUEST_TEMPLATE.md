fa2627ed1de34451e7e0f57108a9c2914b5196c4
smiska <ms@creygames.com>
2020-12-04 10:55:39 +0100

    Revert "deploy: 7aeaf2fa3ec9290660b23ed68581847737d29877"
    This reverts commit 4afdb0ced10be42a0d9c70f2cf525cec4c2c6f60.

---
+++ PULL_REQUEST_TEMPLATE.md
@@ -0,0 +1,5 @@
+## What does this PR do?
+
+## How should it be tested _(related URLs, feature keys)_ ?
+
+## Related task(s)
---
+++ dist/index.js
@@ -0,0 +1,9 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.Button = void 0;
+var _componentsButton = './components/Button',
+    Button = _componentsButton.Button;
+exports.Button = Button;
